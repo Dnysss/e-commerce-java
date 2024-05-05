b349b28561f7595546cc6ad3710fbe7ff4e7c3ad
Dnysss <denysantos2014@hotmail.com>
2024-05-05 16:09:10 -0300

    Service layer, component registration

---
+++ src/main/java/com/example/fullstack/services/UserService.java
@@ -0,0 +1,25 @@
+package com.example.fullstack.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.fullstack.entities.User;
+import com.example.fullstack.repositories.UserRepository;
+
+@Service
+public class UserService {
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
