b3559aade9eed76aa57b3728840151c5cbc9755d
Dnysss <denysantos2014@hotmail.com>
2024-05-06 16:32:06 -0300

    created CategoryEntitie, Service and Resource

--- src/main/java/com/example/fullstack/config/TestConfig.java
+++ src/main/java/com/example/fullstack/config/TestConfig.java
@@ -8,9 +8,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.example.fullstack.entities.Category;
 import com.example.fullstack.entities.Order;
 import com.example.fullstack.entities.User;
 import com.example.fullstack.entities.enums.OrderStatus;
+import com.example.fullstack.repositories.CategoryRepository;
 import com.example.fullstack.repositories.OrderRepository;
 import com.example.fullstack.repositories.UserRepository;
 
@@ -22,9 +24,18 @@ public class TestConfig implements CommandLineRunner {
 	
 	@Autowired
 	private OrderRepository orderRepository;
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
+		Category cat1 = new Category(null, "Electronics");
+		Category cat2 = new Category(null, "Books");
+		Category cat3 = new Category(null, "Computers");
+		
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+		
 		User u1 = new User(null, "Denys", "denys@gmail.com", "99999999", "123456");
 		User u2 = new User(null, "Maira Brown", "maria@gmail.com", "88888888", "123456");
 		
@@ -34,6 +45,7 @@ public class TestConfig implements CommandLineRunner {
 		
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+		
 	}
 	
 	
