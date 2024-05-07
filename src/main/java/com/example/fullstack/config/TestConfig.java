5c64bbbbaa7c0a4b9fa3bc0e07ec97e1ea93dcc4
Dnysss <denysantos2014@hotmail.com>
2024-05-07 10:28:35 -0300

    Payment, one-to-one association

--- src/main/java/com/example/fullstack/config/TestConfig.java
+++ src/main/java/com/example/fullstack/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.example.fullstack.entities.Category;
 import com.example.fullstack.entities.Order;
 import com.example.fullstack.entities.OrderItem;
+import com.example.fullstack.entities.Payment;
 import com.example.fullstack.entities.Product;
 import com.example.fullstack.entities.User;
 import com.example.fullstack.entities.enums.OrderStatus;
@@ -78,6 +79,11 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 	
 	
